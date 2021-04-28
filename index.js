30bb34036de49728faa08545a47ae4465bca5cf0
esenturova <aiperi.esenturova@iaau.edu.kg>
2021-04-28 09:47:47 +0600

    i added new project

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+   
+    loop: true,
+    slidesPerView:4,
+    slidesPerGroup:1,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+  
+    
+  });
